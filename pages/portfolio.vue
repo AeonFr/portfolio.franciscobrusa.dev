5378d5a0cfcac5222ad9a4f7a0979651c03eddff
AeonFr <frankno.94@gmail.com>
2018-08-09 22:30:57 +0200

    changes in portfolio: reorganized and added content

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -2,48 +2,51 @@
   <main id="portfolio" class="container">
     <h1>Portfolio</h1>
 
-    <p>Sitio web para <em>Guías: Gestión Integral en Arquitectura Sustentable</em>. <a href="https://guiasarq.netlify.com/" target="_blank">Link</a></p>
+    <li>Sitio web para <em>Guías: Gestión Integral en Arquitectura Sustentable</em>. <a href="https://guiasarq.netlify.com/" target="_blank">Link</a></li>
 
     <img src="~/assets/portfolio/guiasarq-mockup.jpg">
 
-    <p>Logotipo e identidad corporativa</p>
-    <img src="~/assets/portfolio/bertolina-mockup.png">
+    <li>Logotipo e identidad corporativa para <a href="https://www.bertolinabikes.com.ar/" target="_blank">Bertolina Bikes</a></li>
     <img src="~/assets/portfolio/logo-animado.gif">
+    <img src="~/assets/portfolio/bertolina-mockup.png">
 
-    <p>Sitio web de <em>e-commerce</em> responsivo y optimizado para conecciones lentas. <a href="https://www.bertolinabikes.com.ar/" target="_blank">Link</a></p>
+    <li>Sitio web <em>responsive</em> y optimizado para conecciones lentas. <a href="https://www.bertolinabikes.com.ar/" target="_blank">Link</a>.
+    <br>Para el sitio web se creó un sistema de diseño personalizado, documentado en <a href="https://www.bertolinabikes.com.ar/meta/manual-de-identidad/" target="_blank">esta guía de estilo</a>.</li>
+    <img src="~/assets/portfolio/bertolina-mockup-2.jpg">
 
-    <!--img src="~/assets/portfolio/bertolina-mockup.jpg">
-
-    <p>Tapa de libro, ilustración con xilografía en blanco y negro, posteriormente coloreada de forma digital.</p>
-
-    <img src="~/assets/portfolio/tapa-libro-redhondinho.jpg">
-
-    <p>Ilustración digital.<br><small>Bob Dylan en dos etapas distintas de su carrera, representado a través de sus frases.</small></p>
-
-    <img src="~/assets/portfolio/dos-ilustraciones.jpg">
-
-    <p>Afiche para Facultad de Arte y Diseño Lino Enea Spilmbergo.<br><small>Fotografía por <a href="https://deathtothestockphoto.com/" target="_blank">deathtostockphoto</a>.</small></p>
-
-    <img src="~/assets/portfolio/afichespili-mockup.jpg">
-
-    <p>Flyer. (ad-honorem)</p>
-    
-    <img src="~/assets/portfolio/folleto-juice-bar.png">
-
-    <p>Menú de bar, con ilustraciones. (ad-honorem)</p>
-
-    <img src="~/assets/portfolio/juice-bar.jpg"-->
-
-    <p>Collage digital.<br><small>Licencia <a href="https://creativecommons.org/licenses/by/2.0/es/" target="_blank">Creative Commons 2.0</a>.</small></p>
-
-    <img src="~/assets/portfolio/collage-creatividad.jpg">
-
-    <p>Temas para <a href="//blockity.co" target="_blank">blockity</a>, aplicación web para la creación de sitios web.</p>
+    <li>Temas para <a href="//blockity.co" target="_blank">blockity</a>, aplicación web para la creación de sitios web.</li>
 
     <img src="~/assets/portfolio/blockity-templates.jpg">
 
+    <h2 class="mt4">Trabajos de diseño gráfico</h2>
+    <p class="f6">Estos trabajos fueron realizados <em>ad-honorem</em> durante mi cursado en la universidad.</p>
 
-    <p><b>Más proyectos</b> en Instagram, CodePen y GitHub.</p>
+    <div class="mt4"></div>
+
+    <li>Tapa de libro, ilustración xilográfica en blanco y negro, posteriormente coloreada de forma digital.</li>
+
+    <img src="~/assets/portfolio/tapa-libro-redhondinho.jpg">
+
+    <li>Ilustración digital.<br><small>Bob Dylan en dos etapas distintas de su carrera, representado a través de sus frases.</small></li>
+
+    <img src="~/assets/portfolio/dos-ilustraciones.jpg">
+
+    <li>Afiche para Facultad de Arte y Diseño Lino Enea Spilmbergo.<br><small>Fotografía por <a href="https://deathtothestockphoto.com/" target="_blank">deathtostockphoto</a>.</small></li>
+
+    <img src="~/assets/portfolio/afichespili-mockup.jpg">
+
+    <li>Flyer para Juice Bar (Córdoba).</li>
+    
+    <img src="~/assets/portfolio/folleto-juice-bar.png">
+
+    <li>Menú de bar, con ilustraciones.</li>
+
+    <img src="~/assets/portfolio/juice-bar.jpg">
+
+
+
+    <h2 class="mt4">Más...</h2>
+    <p>...en Instagram, CodePen y GitHub.</p>
 
     <icons></icons>
 
