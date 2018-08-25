e8d23b6562bd87f98bbee6aa77878994d85db902
AeonFr <frankno.94@gmail.com>
2018-08-25 17:03:57 +0200

    mejor presentación

--- pages/portfolio/storekey.vue
+++ pages/portfolio/storekey.vue
@@ -1,19 +1,46 @@
 <template>
   <main class="container">
 
-    <h1>Proyecto: Storekey</h1>
 
-    <negate-container>
+
+
+
+    <negate-container class="flex-ns items-start ph2 pl5-ns">
+      
+      <div>
+        <h1>Aplicación Web de gestión de clientes</h1>
+        <p>Contabilidad y envío de notificaciones por E-mail y Whatsapp, todo desde esta App creada a medida.</p>
+        <a href="https://storekey-demo.francisco-cano.com/app" target="_blank" rel="nofollow"
+           class="dib f4 link bg-blue br-pill pv2 ph3 white">
+          Demo
+        </a>
+      </div>
       <img src="~/assets/portfolio/storekey.jpg">
     </negate-container>
 
 
-    <p class="f4">Creación una aplicación web de gestión de clientes y envío de emails para un pequeño negocio. La App, 100% responsive, fue escrita en Vue.js con back-end en WordPress.</p>
+    <ul class="list pa0 f4">
+      <li>100% Responsive</li>
+      <li><em>Front-end</em> en Vue.js</li>
+      <li><em>Back-end</em> en vanilla PHP y funciones de WordPress</li>
+    </ul>
 
-    <p>Permitió automatizar tareas como imprimir fichas de cliente, ahorrando tiempo - pero por otro lado permitió ahorrar dinero al generar emails de forma masiva y evitar envió de cartas por correo (tradicional). 100% responsive; actualmente se está re-escribiendo el <em>back-end</em> con el <abbr>ORM</abbr> de Laravel, con planes de ser lanzado como programa <em>open-source</em> (<a href="https://github.com/AeonFr/storekeyCRM" target="_blank">repositorio de GitHub</a>).</p>
+    <p class="lh-copy">
+      Permitió automatizar tareas como imprimir fichas de cliente,
+      ahorrando tiempo y dinero al generar emails de forma masiva
+      y evitar envió de cartas por correo (tradicional).
+    </p>
+    <p>
+      Actualmente se está re-escribiendo: el front-end será una <abbr>PWA</abbr>
+      y el <em>back-end</em> funcionará con el <abbr>ORM</abbr> de Laravel.
+      El código se mantiene en
+      <a href="https://github.com/AeonFr/storekeyCRM" target="_blank">
+        este repositorio de GitHub
+      </a>.
+    </p>
 
     <p class="pa2 shadow-2">
-      <a href="http://storekey-demo.francisco-cano.com/app" target="_blank" rel="nofollow">Demo</a>
+      <a href="https://storekey-demo.francisco-cano.com/app" target="_blank" rel="nofollow">Demo</a>
       <br> Contraseña: <code>password</code>
       <br><span class="f6 gray">Nota: Algunas funciones, como la generación de PDFs, no están disponibles en la demo.</span>
     </p>
